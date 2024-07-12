4423babf8d6611b4d03d214b4c0f114f2918812d
rvalencia <rva.valencia@gmail.com>
2024-07-12 16:55:00 -0400

    setFavorite commit

--- src/components/MyPlaylist.jsx
+++ src/components/MyPlaylist.jsx
@@ -10,28 +10,39 @@ export function MyPlaylist() {
      */
     const urlRef = useRef() // Esto es un hook que asociamos a un input
     const favoriteRef = useRef()
-
+    const [mensaje, setMensaje] = useState('');
     /**
      * useState es un hook que permite vigilar el ciclo de vida de un elemento
      */
     const [songList, setSongList] = useState([
-        {id: uuid(), url:"3fn4HfVz5dhmE0PG24rh6h", favorite:true},
-        {id: uuid(), url:"2UJcKiJxNryhL050F5Z1Fk", favorite:false},
-        {id: uuid(), url:"5B4PYA7wNN4WdEXdIJu58a", favorite:true},
-        {id: uuid(), url:"1XqzXn8a7Z15Z5tBSouXcH", favorite:false}
+        { id: uuid(), url: "3fn4HfVz5dhmE0PG24rh6h", favorite: true },
+        { id: uuid(), url: "2UJcKiJxNryhL050F5Z1Fk", favorite: false },
+        { id: uuid(), url: "5B4PYA7wNN4WdEXdIJu58a", favorite: true },
+        { id: uuid(), url: "1XqzXn8a7Z15Z5tBSouXcH", favorite: false }
     ]);
 
     /**Funcion para agregar canciones */
-    function addSong(){
+    function addSong() {
         console.log('Presionando boton agregar')
         const url = urlRef.current.value;
         const favorite = favoriteRef.current.checked;
         console.log(`Se escribio ${url} ${favorite}`);
+
+        if(url.trim() === ''){
+            setMensaje('Campos vacios');
+
+            setTimeout(() => {
+                setMensaje('');
+            }, 3000);
+
+            return
+        }
+
         /**Con los datos obtenidos podemos crear un objeto song */
         const newSong = {
-            id:uuid(),
-            url:url,
-            favorite:favorite
+            id: uuid(),
+            url: url,
+            favorite: favorite
         }
 
         const newSongList = [...songList, newSong]
@@ -40,28 +51,43 @@ export function MyPlaylist() {
         alert('Se agrego cancion');
     }
 
-    function removeSongs(){
+    function removeSongs() {
         const updatedList = songList.filter(song => song.favorite === true);
         console.log(updatedList);
         setSongList(updatedList);
         alert('Se eliminaron no favoritas');
     }
-    
+
+    function setFavorite(id) {
+        console.log(id);
+        const newList = [...songList];
+        console.log(newList);
+        const song = newList.find(c => c.id === id);
+        console.log(song);
+        song.favorite = !song.favorite;
+        console.log(song);
+        setSongList(newList);
+    }
+
 
     return (
         <div className="container">
             <h1>My favorite songs</h1>
             <div className="d-flex">
-                <input ref={urlRef} className="form-control m-2" type="text" placeholder="URL Song"/>
-                <input ref={favoriteRef} id="checkbox" className="m-2" type="checkbox"/>
+                <input ref={urlRef} className="form-control m-2" type="text" placeholder="URL Song" />
+                <input ref={favoriteRef} id="checkbox" className="m-2" type="checkbox" />
                 <label className="m-2">Favorite?</label>
                 <button className="btn btn-success m-2" onClick={addSong}><i class="bi bi-check-circle"></i></button>
                 <button className='btn btn-danger m-2' onClick={removeSongs}><i class="bi bi-trash"></i></button>
             </div>
+            <div class="alert alert-danger mt-2" role="alert" hidden={!mensaje}>
+                {mensaje}
+            </div>
             <div>
                 {
-                    songList.map(song => <SpotifyPlayer urlSong={song.url} favorite={song.favorite} key={song.id}/>)
-                }                
+
+                    songList.map(song => <SpotifyPlayer setFavorite={setFavorite} song={song} key={song.id} />)
+                }
             </div>
         </div>
     )
--- src/components/SpotifyPlayer.jsx
+++ src/components/SpotifyPlayer.jsx
@@ -1,19 +1,23 @@
-export function SpotifyPlayer({urlSong, favorite}) {
-    const url = "https://open.spotify.com/embed/album/" + urlSong
+export function SpotifyPlayer({song, setFavorite}) {
+    const { id, url, favorite } = song; /** {id: uuid, url:"asdadasdas", favorite:true} */
+
+    const newFavorite = () => setFavorite(id);
+
+    const urlSong = "https://open.spotify.com/embed/album/" + url
 
     if (favorite) {
         return (
             <div className="d-flex my-2">
-                <iframe src={url} width="100%" height="80" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-                <button className="btn"><i class="bi bi-star-fill"></i></button>
+                <iframe src={urlSong} width="100%" height="80" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+                <button className="btn" onClick={newFavorite}><i class="bi bi-star-fill"></i></button>
             </div>
         )
     }
 
     return (
-        <div className="d-flex">
-            <iframe src={url} width="100%" height="80" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-            <button className="btn"><i class="bi bi-star"></i></button>
+        <div className="d-flex my-2">
+            <iframe src={urlSong} width="100%" height="80" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+            <button className="btn" onClick={newFavorite}><i class="bi bi-star"></i></button>
         </div>
     )
 }
